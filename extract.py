06a257addb566dd5ed77170a83b7ef3764134391
gitpizzanow <sahraouitarikziad@gmail.com>
2025-07-10 07:30:49 -1100

    Update extract.py

--- extract.py
+++ extract.py
@@ -5,10 +5,10 @@ import os
 X = []
 y = []
 
-# Set the correct base path
+
 base_path = "data/train"
 
-# Loop through 'cats' and 'dogs' folders
+
 for label_name, label_value in [('cats', 1), ('dogs', 0)]:
     folder = os.path.join(base_path, label_name)
     for filename in os.listdir(folder):
@@ -19,10 +19,8 @@ for label_name, label_value in [('cats', 1), ('dogs', 0)]:
             X.append(arr)
             y.append(label_value)
 
-# Convert lists to numpy arrays
 X = np.array(X)
 y = np.array(y)
 
-# Save to .txt files
 np.savetxt('X_data.txt', X, delimiter=",")
 np.savetxt('y_data.txt', y, fmt="%d", delimiter=",")
